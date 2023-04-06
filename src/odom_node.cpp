ad9cf06a54c570d350d1497bace1b528b5d4720a
Andrea Giuffrida <andrea.giuffrida@live.it>
2023-04-06 21:25:20 +0200

    Second commit

--- src/odom_node.cpp
+++ src/odom_node.cpp
@@ -1,10 +1,14 @@
 #include "ros/ros.h"
 #include "nav_msgs/Odometry.h"
 #include "first_project/Odom.h"
+#include <time.h>
+#include "std_msgs/String.h"
+
+#include <sstream>
 
 class odom{
     nav_msgs::Odometry odom_message;
-
+    first_project::Odom cust_msg;
 
 
 private:
@@ -16,10 +20,19 @@ private:
 public:
     odom(){
         sub = n.subscribe("/steer_speed", 1000, &odom::callback, this);
+        pub = n.advertise<first_project::Odom>("/custom_odometry", 1000);  
     }
 
     void callback(const geometry_msgs::Quaternion& msg){
+        /*
+        cust_msg.x = msg.x;
+        cust_msg.y = msg.y;
+        cust_msg.th = msg.w;
+        cust_msg.timestamp = std::to_string(ros::Time::now().toSec());   
+        pub.publish(cust_msg);
+        */
 
+       ROS_INFO("Hello");
     }
 };
 
